f14dcb5c7034d4e97160cfed01558240156e73d8
uvejsazizi <aziziuvejs@gmail.com>
2026-01-17 14:47:14 +0100

    1
    1

---
+++ moduli2/index.php
@@ -0,0 +1,20 @@
+<?php
+ $school_name ="Digital School";
+
+ echo ("i go to $school_name")."<hr>";
+
+ $numri = 2;
+
+ echo ("i am in level number $numri")."<hr>";
+
+$thestring ="klasa ma e mir ";
+
+echo strlen($thestring)."<hr>";
+
+echo str_word_count($thestring)."<hr>";
+
+echo str_replace("mir", "zhurmshme", $thestring). "<hr>";
+
+echo strrev($thestring)."<hr>";
+ 
+?>
